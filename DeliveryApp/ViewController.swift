da752d7431177e8bdc076fb044774da68ce43750
Aradhana Ray <aradhanaray@ritwik.local>
2018-07-24 12:03:04 +0530

    Initial Commit

---
+++ DeliveryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeliveryApp
+//
+//  Created by Aradhana Ray on 24/07/18.
+//  Copyright © 2018 Aradhana Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
